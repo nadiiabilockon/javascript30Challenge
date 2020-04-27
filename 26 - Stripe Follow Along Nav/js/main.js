fc5e9855a93bf190945a3dd4eee6ee1b0c1e9d49
nadiiabilockon <11071995nad@gmail.com>
2020-04-27 16:14:32 +0300

    Stripe Follow Along Nav

---
+++ 26 - Stripe Follow Along Nav/js/main.js
@@ -0,0 +1,45 @@
+const triggers = document.querySelectorAll(".cool > li");
+const background = document.querySelector(".dropdownBackground");
+const nav = document.querySelector(".top");
+
+function handleEnter() {
+    this.classList.add("trigger-enter");
+    setTimeout(
+        () =>
+            this.classList.contains("trigger-enter") &&
+            this.classList.add("trigger-enter-active"),
+        150
+    );
+    background.classList.add("open");
+
+    const dropdown = this.querySelector(".dropdown");
+    const dropdownCords = dropdown.getBoundingClientRect();
+    const navCords = nav.getBoundingClientRect();
+
+    const coords = {
+        height: dropdownCords.height,
+        width: dropdownCords.width,
+        top: dropdownCords.top - navCords.top,
+        left: dropdownCords.left - navCords.left,
+    };
+
+    background.style.setProperty("width", `${coords.width}px`);
+    background.style.setProperty("height", `${coords.height}px`);
+    background.style.setProperty(
+        "transform",
+        `translate(${coords.left}px, ${coords.top}px)`
+    );
+}
+
+function handleLeve() {
+    this.classList.remove("trigger-enter", "trigger-enter-active");
+    background.classList.remove("open");
+}
+
+triggers.forEach((trigger) =>
+    trigger.addEventListener("mouseenter", handleEnter)
+);
+
+triggers.forEach((trigger) =>
+    trigger.addEventListener("mouseleave", handleLeve)
+);
